491cc1559474e74481e676f1a8488522ea7cfafc
Phil Pepin <ppepin34@gmail.com>
2022-03-26 19:25:59 -0400

    preliminary html and css

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// define any constants/ global variables
+
+//beginning quiz
+//change html to new format, begin timer
+
+//timer
+
+// what happens when you answer a question
+
+// right answer
+    //special class/id
+
+// wrong answer
+//subtract from timer
+
+//move to next question
+
+//end of quiz
+//save timer state
+//form for saving scores
+
+//event listeners--- these go somewhere
+
+//save scores
+//order high to low
+//maybe save top 10 only?
+    // ol -> save index positions 0-9
+//delete scores
