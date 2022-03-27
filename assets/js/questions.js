6df41984b9c36dc320f0f4b07065f3ee8d6bcfac
Phil Pepin <ppepin34@gmail.com>
2022-03-27 16:01:06 -0400

    timer starts on quiz start, stops when at 0

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Commonly used data types DO NO include:",
+        answer: "alerts",
+        options: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers",
+        ]
+    },
+    {
+        numb: 2,
+        question: "The condition in an if/else statement is enclosed with _______.",
+        answer: "parenthesis",
+        options: [
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in JavaScript can be used to store ________.",
+        answer: "all of the above",
+        options: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above",
+        ]
+    },
+    {
+        numb: 4,
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answer: "quotes",
+        options: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis"
+        ]
+    },
+    {
+        numb: 5,
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer: "console.log",
+        options: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log"
+        ]
+    },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,80 @@
 // define any constants/ global variables
-
-//beginning quiz
-//change html to new format, begin timer
+var time  = document.getElementById("time");
+var timeLeft = "";
+var highScores = [];
 
 //timer
+var timer = function() {
+    timeLeft = 74;
+
+    //runs var timeLeft through countdown
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            console.log(timeLeft)
+            time.innerHTML = timeLeft;
+            timeLeft--;
+        } else {
+            time.innerHTML = 0;
+            clearInterval(timeInterval);
+            //endQuiz();
+        };
+    }, 1000)
+}
+
+//beginning quiz
+var quizStart = function() {
+    //debugger
+    //change html to new format, begin timer
+    var splash = document.getElementById("splash")
+    splash.classList.add("hidden");
+    splash.classList.remove("visible");
+
+    var quiz = document.getElementById("quiz")
+    quiz.classList.remove("hidden");
+    quiz.classList.add("visible");
+}
+     {   
+
+    //dynamically generate questions
+    var newQuestion = function(){};
+
+    var answer = function(){
+        // right answer
+        if (event.target.id ="correct"){
+            //move to next question
+            newQuestion();
+            footer.appendChild(correct);
+        }
+        else {
+            // wrong answer
+            //subtract from timer
+            timeLeft = timeLeft - 10;
+            
+            //move to next question
+            newQuestion
+            footer.appendChild(incorrect);
+                }
+            
+        }
+
 
 // what happens when you answer a question
+//document.getElementsByClassName("answers").addEventListener("click",answer)
 
-// right answer
-    //special class/id
-
-// wrong answer
-//subtract from timer
-
-//move to next question
-
+}
 //end of quiz
+
+//function endQuiz()
+
+
 //save timer state
 //form for saving scores
 
 //event listeners--- these go somewhere
-
+ document.getElementById("start-quiz").addEventListener("click", () => {
+     quizStart();
+     timer();
+ });
 //save scores
 //order high to low
 //maybe save top 10 only?
