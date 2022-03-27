d1eac6c2148fb7b150b6c6a4b442957882b4dcd9
Phil Pepin <ppepin34@gmail.com>
2022-03-27 19:42:35 -0400

    first quiz opens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // define any constants/ global variables
-var time  = document.getElementById("time");
+var time = document.getElementById("time");
 var timeLeft = "";
 var highScores = [];
+// var answerButton = document.getElementsByClassName("answer-button")
 
 //timer
-var timer = function() {
+var timer = function () {
     timeLeft = 74;
 
     //runs var timeLeft through countdown
     var timeInterval = setInterval(function () {
         if (timeLeft > 0) {
-            console.log(timeLeft)
             time.innerHTML = timeLeft;
             timeLeft--;
         } else {
@@ -22,46 +22,45 @@ var timer = function() {
 }
 
 //beginning quiz
-var quizStart = function() {
+var quizStart = function () {
     //debugger
     //change html to new format, begin timer
     var splash = document.getElementById("splash")
     splash.classList.add("hidden");
     splash.classList.remove("visible");
 
-    var quiz = document.getElementById("quiz")
+    var quiz = document.getElementById("quiz-1")
     quiz.classList.remove("hidden");
     quiz.classList.add("visible");
 }
-     {   
 
-    //dynamically generate questions
-    var newQuestion = function(){};
+var answer = function () {
 
-    var answer = function(){
-        // right answer
-        if (event.target.id ="correct"){
-            //move to next question
-            newQuestion();
-            footer.appendChild(correct);
-        }
-        else {
-            // wrong answer
-            //subtract from timer
-            timeLeft = timeLeft - 10;
-            
-            //move to next question
-            newQuestion
-            footer.appendChild(incorrect);
-                }
-            
-        }
+    console.log(element.classList)
+    // right answer
+    if (element.classList == "correct") {
+        //move to next question
+        newQuestion();
+        footer.appendChild(correct);
+    }
+    else {
+        // wrong answer
+        //subtract from timer
+        timeLeft = timeLeft - 10;
+
+        //move to next question
+        newQuestion
+        footer.appendChild(incorrect);
+
+        console.log(timeLeft);
+    }
+
+}
 
 
 // what happens when you answer a question
 //document.getElementsByClassName("answers").addEventListener("click",answer)
 
-}
 //end of quiz
 
 //function endQuiz()
@@ -71,10 +70,14 @@ var quizStart = function() {
 //form for saving scores
 
 //event listeners--- these go somewhere
- document.getElementById("start-quiz").addEventListener("click", () => {
-     quizStart();
-     timer();
- });
+document.getElementById("start-quiz").addEventListener("click", () => {
+    quizStart();
+    timer();
+});
+
+document.querySelectorAll("answer-button").forEach(element => {
+    element.addEventListener("click", answer(element.classList))
+});
 //save scores
 //order high to low
 //maybe save top 10 only?
